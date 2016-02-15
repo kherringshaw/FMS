b45a2548c07a3ba999441ec0926e1824483bf8b5
undercover99 <tararaj21@gmail.com>
2016-02-15 12:38:26 -0600

    Updated Unit to extend Facility
    Commented out unnecessary attributes & methods

--- src/com/fms/model/facility/Unit.java
+++ src/com/fms/model/facility/Unit.java
@@ -1,24 +1,27 @@
 package com.fms.model.facility;
 
-public class Unit {
-	private String unit;
-	private String unitID;
+public class Unit extends Facility {
+	//private String unit;
+	//private String unitID;
+	private List<Use> usage;
 	
-	public Unit(){}
+	public Unit(){	}
 	
-	public String getunit() {
+	/* public String getunit() {
 		return unit;
 	}
 	public void setunit(String unit) {
 		this.unit = unit;
-	}	
+	}
+	*/
 	
-	public String getunitID() {
+	/* public String getunitID() {
 		return unitID;
 	}
 	public void setunitID(String unitID) {
 		this.unitID = unitID;
 	}
+	*/
 
 
 }
